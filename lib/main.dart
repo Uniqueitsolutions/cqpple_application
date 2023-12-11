53fbedad1cdc3ee1965baf01336148cb38289c67
vivek <vivek.007shah@gmail.com>
2023-12-11 19:31:24 +0530

    FCM android added

--- lib/main.dart
+++ lib/main.dart
@@ -13,20 +13,28 @@ import 'package:bath_service_project/pages/service_status_page.dart';
 import 'package:bath_service_project/pages/login_verification_page.dart';
 
 import 'package:bath_service_project/pages/splashscreen_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  // FirebaseMessaging.onBackgroundMessage(backgroundHandler);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // final PushNotificationService _notificationService = PushNotificationService();
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // _notificationService.initialize();
+
     return MaterialApp(
         title: 'Cqpple',
         debugShowCheckedModeBanner: false,
